44f0b863f4c957b94029f56228705878eb3723c1
Wilson <ngerchuyang@gmail.com>
2011-05-13 14:19:06 +0800

    Fixed login/registration bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,25 +1,38 @@
 class RegistrationsController < Devise::RegistrationsController
- def create
-    #super
-    build_resource
 
-    if resource.save
-      set_flash_message :notice, :signed_up
-      # Email admin
-      UserMailer.join_mail(resource, User.count).deliver
-      # Send welcome email
-      UserMailer.welcome_mail(resource.email).deliver
-      sign_in_and_redirect(resource_name, resource)
-    else
-      clean_up_passwords(resource)
-      render_with_scope :new
-    end
-    
-    session[:auth] = nil unless @user.new_record?
-    #UserMailer.join_mail(@user, User.count).deliver if session[:auth]
+  # GET /resource/sign_up
+  def new
+    build_resource({})
+    render_with_scope :new
   end
   
-  private
+  # POST /resource
+  def create
+    build_resource
+    
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_in(resource_name, resource)
+        
+        UserMailer.join_mail(resource, User.count).deliver
+        # Send welcome email
+        UserMailer.welcome_mail(resource.email).deliver
+        
+        respond_to do |format|
+          format.html {respond_with resource, :location => redirect_location(resource_name, resource)}
+          format.json  { render :json => resource, :callback => params[:callback] }
+        end
+      else
+        set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords(resource)
+      respond_with_navigational(resource) { render_with_scope :new }
+    end
+  end
   
   def build_resource(*args)
     super
